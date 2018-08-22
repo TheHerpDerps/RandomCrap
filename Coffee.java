32919f18632160c9cc97ded904077c1d0c749410
Nikolai <nikolai.solo.b@gmail.com>
2018-08-22 13:45:10 -0500

    drank some coffee

---
+++ Coffee.java
@@ -0,0 +1,14 @@
+Public class Coffee{
+    Int cups;
+    Int mugs;
+    Int AmountOfCoffee;
+
+    public Coffee(){
+        cups = 3;
+        mugs = 4;
+        AmountOfCoffee = mugs * cups;
+    }
+    public void HowMuchDidIDrink(){
+        return AmountOfCoffee;
+    }
+}
