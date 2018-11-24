16e91eb2b4b337845c11b479047e732f0644361b
Nikolai <nikolai.solo.b@gmail.com>
2018-11-23 22:33:09 -0600

    added mug choice

--- Coffee.java
+++ Coffee.java
@@ -1,17 +1,23 @@
-Public class Coffee{
-    Int cups;
-    Int mugs;
+import java.util.Random;
+public class Coffee{
+    int type;
+    Int[] cups;
+    Int Fillings;
     Int AmountOfCoffee;
     
 
     public Coffee(){
-        cups = 6;
-        mugs = 4;
-        AmountOfCoffee = mugs * cups;
+        type = rand.nextInt(2) + 0;
+        cups[0] = 6;
+        cups[1] = 8;
+        cups[2] = 12;
+        Fillings = 4;
+        AmountOfCoffee = mugs * cups[type];
     }
     public void HowMuchDidIDrink(){
-        return AmountOfCoffee;
+        return AmountOfCoffee + "oz";
     }
     
     public void BathroomTime(double Time){
+    }
 }
